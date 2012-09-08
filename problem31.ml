b0a3865a428b95486c63b6a1866f6f3fade592af
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-09-08 23:18:30 +0300

    back to electomagnetic fields :(

---
+++ problem31.ml
@@ -0,0 +1,17 @@
+let rec withOne num coin restcoins acc =
+        if (coin=1) then acc+1
+        else
+          match num with 
+            | i when i < coin -> acc
+            | i -> withOne (num-coin) coin restcoins (acc+(ways (num-coin) restcoins))
+
+and ways num coins =      
+    let rec aux num coins acc = 
+        match coins with 
+         |   [] -> acc
+         |   head::tail ->  aux num tail (acc + (withOne num head tail 0))
+    in
+    aux num coins 0
+;;
+
+ways 200 [200;100;50;20;10;5;2;1];;
