8b440272cc809963318d346735250c56d82a49ab
Nick Giannarakis <nick.giannarakis@gmail.com>
2012-10-03 20:41:09 +0300

    Problem43 is now powered by Euler-lib :) (also gitignore was updated but w/e right?)

--- problem43.ml
+++ problem43.ml
@@ -1,31 +1,7 @@
-let swap ind1 ind2 tbl =
-  let temp = tbl.(ind1) in
-    tbl.(ind1) <- tbl.(ind2);
-    tbl.(ind2) <- temp 
-;;
-
-let rec reverse ind1 ind2 tbl =
-  match (ind2-ind1) with 
-    | 0 -> ()
-    | 1 -> swap ind1 ind2 tbl
-    | _ -> swap ind1 ind2 tbl; 
-           reverse (ind1 + 1) (ind2 - 1) tbl
-;;
-
-
-let int_pow a b = truncate ((float a) ** (float b));;
-
-let fact num =
-  let rec aux i acc =
-    match i with
-      | i when (i=num) -> acc*num
-      | i -> aux (i+1) (acc*i)
-  in
-    aux 2 1
-;;
+open Euler;;
 
 let valid d =
-  let num i = (d.(i)*100+d.(i+1)*10+d.(i+2)) in 
+  let num i = ((int_of_baseTypes d.(i))*100 + (int_of_baseTypes d.(i+1))*10 + (int_of_baseTypes d.(i+2))) in 
     ((((num 1) mod 2) = 0) &&
      (((num 2) mod 3) = 0) &&
      (((num 3) mod 5) = 0) &&
@@ -35,32 +11,20 @@ let valid d =
      (((num 7) mod 17) = 0))
 ;;
 
-
-
-let permutations digits =
-  let perm = Array.make digits 0 in 
-  let k = ref 0 in 
-  let l = ref 0 in 
-  let sum = ref 0 in
-  let num = ref 0 in 
-  let bound = fact digits in
-    Array.iteri (fun ind _ -> perm.(ind) <- ind) perm; 
-    for i = 2 to bound do
-      k := (digits-2);
-      while (perm.(!k) >= perm.((!k)+1)) do
-        k := (!k) - 1; 
-      done;
-      l := (digits-1);
-      while (perm.(!k) >= perm.(!l)) do
-        l := (!l) - 1; 
-      done;
-      swap (!k) (!l) perm;
-      reverse ((!k)+1) (digits-1) perm;
-      num := 0;
-      Array.iteri (fun ind value ->  (num:= (!num) + value*(int_pow 10 (digits-ind-1)))) perm;
-      if (valid perm) then sum:=(!sum)+(!num);
-    done;
-    (!sum)
+let solve = 
+    let xs = [Int 0; Int 1; Int 2; Int 3; Int 4; Int 5; Int 6; Int 7; Int 8; Int 9] in
+    let perm = Array.of_list xs in
+    let digits = Array.length perm in
+    let num = ref 0 in
+    let sum = ref 0 in
+    let rec aux() =
+        match try Some (Permutations.nextPermutation perm) with Permutations.LastPermutation -> None with
+            | Some _ -> Array.iteri (fun ind value ->  (num := (!num) + (int_of_baseTypes value)*(MathTools.int_pow 10 (digits-ind-1)))) perm;
+                        if (valid perm) then sum := (!sum) + (!num);
+                        num := 0;
+                        aux ();
+            | None ->  (!sum)
+    in
+        aux ()
 ;;
 
-let solve = permutations 10
