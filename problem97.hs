e8259c635a6d68e17816698c022e1aae0dc40cc8
nickgian <nick.giannarakis@gmail.com>
2013-04-30 16:35:04 +0300

    ...

--- problem97.hs
+++ problem97.hs
@@ -2,9 +2,6 @@
 
 --solve = reverse $ take 10 $ map (`mod` 10) $ takeWhile (>0) $ iterate (`div`10) (28433*(2^7830457) + 1)
 
---solve = (28433 * (2^7830457) + 1) `mod` 10000000000
-
-solve = let a = 2^3915228
-        in ((28433 * a * a * 2) + 1) `mod` 10000000000
+solve = (28433 * (2^7830457) + 1) `mod` 10000000000
 
 main = do putStrLn $ show solve
