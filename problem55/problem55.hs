604d687c4ea4b247d203dcba8d672973bad4f9b0
Zoe P <zoe.paraskevopoulou@gmail.com>
2013-05-01 02:00:10 +0300

    moar haskell

---
+++ problem55/problem55.hs
@@ -0,0 +1,18 @@
+import           Data.List as L
+
+reverseDigits :: Integer -> Integer
+reverseDigits n =
+  let str = show n
+  in read $ reverse str
+
+isNotPalindrome n =
+  let str = show n
+  in str /= reverse str
+
+isLychrel n =
+  50 == (length $ takeWhile isNotPalindrome $ take 50 $ tail $ iterate (\x -> x + reverseDigits x) n)
+
+solve = length $ [n | n <- [1..100000], isLychrel n]
+
+main =
+  do putStrLn $ show solve
