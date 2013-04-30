317ea2eed37528bbf7158239e994e3b0920d7882
nickgian <nick.giannarakis@gmail.com>
2013-04-30 16:34:42 +0300

    ...

--- problem97.hs
+++ problem97.hs
@@ -1,5 +1,10 @@
 {-# OPTIONS_GHC -O2 -optc-O2 #-}
 
-solve = reverse $ take 10 $ map (`mod` 10) $ takeWhile (>0) $ iterate (`div`10) (28433*(2^7830457) + 1)
+--solve = reverse $ take 10 $ map (`mod` 10) $ takeWhile (>0) $ iterate (`div`10) (28433*(2^7830457) + 1)
 
-main = sequence_ [putStr $ show a | a <- solve]
+--solve = (28433 * (2^7830457) + 1) `mod` 10000000000
+
+solve = let a = 2^3915228
+        in ((28433 * a * a * 2) + 1) `mod` 10000000000
+
+main = do putStrLn $ show solve
